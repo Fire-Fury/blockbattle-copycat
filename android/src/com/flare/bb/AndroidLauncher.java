77c2575a86fb7cb47ff664698c4e69507339bf2b
Alexander Wang <alexander.wang2001@gmail.com>
2019-04-11 19:14:39 -0700

    Finished setting up LibGDX project

---
+++ android/src/com/flare/bb/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.flare.bb;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.flare.bb.BlockBattle;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BlockBattle(), config);
+	}
+}
---
+++ desktop/src/com/flare/bb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.flare.bb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.flare.bb.BlockBattle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BlockBattle(), config);
+	}
+}
