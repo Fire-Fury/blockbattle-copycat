e50ac3a91fb833cd02fd0731e6eafacc112343c2
Wang <792574@suhsd-staff.seq.org>
2019-04-16 14:42:44 -0700

    Scaled image to size of window

--- core/src/com/flare/bb/BlockBattle.java
+++ core/src/com/flare/bb/BlockBattle.java
@@ -22,7 +22,7 @@ public class BlockBattle extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0,0);
+		batch.draw(img, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 	}
 	
