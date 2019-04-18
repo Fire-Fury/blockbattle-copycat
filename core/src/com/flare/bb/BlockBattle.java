56b0379fa1156b08624f43dc36f31784e6463059
Wang <792574@suhsd-staff.seq.org>
2019-04-18 14:39:01 -0700

    added text

--- core/src/com/flare/bb/BlockBattle.java
+++ core/src/com/flare/bb/BlockBattle.java
@@ -3,26 +3,39 @@ package com.flare.bb;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class BlockBattle extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	private BitmapFont font;
+	private Sprite sprite;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("android/assets/colors.jpg");
+		sprite = new Sprite(img, 0,0, 1920, 1080);
+		sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		font = new BitmapFont();
+		font.setColor(Color.GOLD);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
-		batch.draw(img, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		{ //FUNCTIONLESS BRACKET. They're here for organizational purposes.
+			sprite.draw(batch);
+			font.draw(batch, "Hello World", 200, 200);
+		}
 		batch.end();
 	}
 	
@@ -30,5 +43,17 @@ public class BlockBattle extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
 	}
 }
