634aa4519e5e2bb853a3d86fb29eae7985cad8fa
Fire-Fury <alexander.wang2001@gmail.com>
2019-05-12 23:46:23 -0700

    first commit from new location

--- desktop/src/com/flare/bb/desktop/DesktopLauncher.java
+++ desktop/src/com/flare/bb/desktop/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.flare.bb.BlockBattle;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Blocknite";
 		// config.width = 960; //540x960 is 1/2 the aspect ratio of most mobile devices that are 1080x1920(reverse of desktop)
